3d4108cc7cc02ef29e4962b730fcdc620a96e4c3
Dariusz Halama <dariuszhalama@gmail.com>
2022-11-19 14:02:38 +0100

    xoxo

---
+++ Assets/Scripts/Installers/DarekInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Installers
+{
+    public class DarekInstaller : MonoInstaller<DarekInstaller>
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/MateuszInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Installers
+{
+    public class MateuszInstaller : MonoInstaller<MateuszInstaller>
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
