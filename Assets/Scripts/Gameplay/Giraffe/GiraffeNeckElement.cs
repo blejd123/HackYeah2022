2892ab318c2ba418114e93dfbbbed6c1d1b87964
Mateusz Biedrzyński <mateuszbiedrzynski@gmail.com>
2022-11-20 05:39:47 +0100

    xoxo

--- Assets/Scripts/Gameplay/Giraffe/GiraffeNeckElement.cs
+++ Assets/Scripts/Gameplay/Giraffe/GiraffeNeckElement.cs
@@ -3,7 +3,27 @@ using DG.Tweening;
 
 public sealed class GiraffeNeckElement : MonoBehaviour
 {
+    [SerializeField] private GameObject _NeckSpherePrefab;
+
     private Tweener _RotationTween;
+    private GameObject _NeckSphere;
+
+    public void Select()
+    {
+        if (_NeckSphere == null)
+        {
+            _NeckSphere = Instantiate(_NeckSpherePrefab, transform);
+        }
+        _NeckSphere.SetActive(true);
+    }
+
+    public void Deselect()
+    {
+        if (_NeckSphere != null)
+        {
+            _NeckSphere.SetActive(false);
+        }
+    }
 
     public void ResetRotation(float duration)
     {
